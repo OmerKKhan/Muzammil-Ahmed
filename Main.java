94af5ceb633da0624ddb764fafea3a39cd28e173
Omer Khan <omerkhan.nu@gmail.com>
2022-06-21 04:08:55 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,53 @@
+// We are building Carkyea
+  Micoroservices we are going to create:
+  Auth, Customer, rides mangagement, tracking service, notifications, wallet,
+
+// Tables
+Auth, Wallet, 
+
+Customer
+Id, Name, Rating
+
+Captain
+Id, Name, rating
+
+Rides
+CustomerId, CaptainId, Fare
+
+select max(rating) from customer
+
+select * from customer innerjoin customerID where 
+
+
+We have string. We need to find if it is a palindrome
+"ssass"
+"dad"
+"schosc"
+
+1)Reverse the string and compare it with original string
+2)Compare index 0 with last index and 
+
+
+Two Sum Problem
+  
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
